4d74327febb7377ad5da0bb60bb8eadcde3fbd66
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-12 14:18:52 +0700

    Belajar Constructor

---
+++ bin/constructor.dart
@@ -0,0 +1,17 @@
+class Person {
+  String name = "Guest";
+  String? address;
+  final String country = "Indonesia";
+
+  Person(String paramName, String paramAddress) {
+    name = paramName;
+    address = paramAddress;
+  }
+}
+
+void main(List<String> args) {
+  var person = Person("Risman", "Jakarta Barat");
+  print(person.name);
+  print(person.address);
+  print(person.country);
+}
