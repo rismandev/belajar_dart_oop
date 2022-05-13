2beb7471511d7fbf23c1ed2e3219cc7bab22d081
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-13 15:53:58 +0700

    Belajar Object Class

---
+++ bin/object_class.dart
@@ -0,0 +1,11 @@
+class Person {}
+
+void main(List<String> args) {
+  // method .toString berasal dari turunan class Object
+  var number = 100;
+  print(number.toString());
+
+  // contoh lain
+  var person = Person();
+  print(person.toString());
+}
