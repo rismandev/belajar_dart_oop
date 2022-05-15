9415463d053f78aa6b8397489888451407682c0b
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-15 15:26:25 +0700

    Belajar Type Check & Casts

---
+++ bin/type_check_casts.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: unnecessary_cast
+
+class Employee {
+  String? name;
+
+  Employee(this.name);
+}
+
+class Manager extends Employee {
+  Manager(String name) : super(name);
+}
+
+class VicePresident extends Manager {
+  VicePresident(String name) : super(name);
+}
+
+void sayHello(Employee employee) {
+  if (employee is VicePresident) {
+    VicePresident vicePresident = employee as VicePresident;
+    print("Hello, VP ${vicePresident.name}");
+  } else if (employee is Manager) {
+    Manager manager = employee as Manager;
+    print("Hello, Manager ${manager.name}");
+  } else {
+    print("Hello, ${employee.name}");
+  }
+}
+
+void main(List<String> args) {
+  // Method polymorphism
+  sayHello(Employee("Risman"));
+  sayHello(Manager("Risman"));
+  sayHello(VicePresident("Risman"));
+}
