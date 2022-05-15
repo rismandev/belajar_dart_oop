d0df949760ce8c91cc2c5c7ef3badd96f7b89281
Risman Abdilah <risman.abdilah@frisidea.com>
2022-05-15 16:58:04 +0700

    Belajar Multiple Interface

--- bin/data/car.dart
+++ bin/data/car.dart
@@ -1,4 +1,6 @@
-class Car {
+// Interface class biasanya dibuat dalam abstract class
+// Interface class 1
+abstract class Car {
   String name = "";
 
   void run() {}
@@ -6,9 +8,19 @@ class Car {
   int getTier() => 0;
 }
 
-class Avanza implements Car {
+// Interface class 2
+abstract class HasBrand {
+  String getBrand();
+}
+
+// implementasi ke interface class bisa dilakukan secara multiple
+// jika ingin mengimplementasi 2 interface, maka gunakan , (koma) sebagai penanda
+class Avanza implements Car, HasBrand {
   @override
   String name = "Avanza";
+
+  @override
+  String getBrand() => "Toyota";
 
   @override
   int getTier() => 4;
---
+++ bin/multiple_interface.dart
@@ -0,0 +1,8 @@
+import 'data/car.dart';
+
+void main(List<String> args) {
+  var avanza = Avanza();
+  avanza.run();
+
+  print(avanza.getBrand());
+}
